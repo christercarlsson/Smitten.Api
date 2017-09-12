a08733be06d339dc40dde24c3a46c4c35ad64d16
Christer Carlsson <ccarlsson@outlook.com>
2017-09-12 20:14:10 +0200

    Add Repository

---
+++ Services/ISmittenRepository.cs
@@ -0,0 +1,24 @@
+﻿using Smitten.Api.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Smitten.Api.Services
+{
+    public interface ISmittenRepository
+    {
+        IEnumerable<Person> GetPeople();
+        Person GetPerson(Guid personId);
+        void AddPerson(Person person);
+        void DeletePerson(Person person);
+        void UpdatePerson(Person person);
+        bool PersonExists(Guid personId);
+        IEnumerable<Smite> GetSmitesForPerson(Guid personId);
+        Smite GetSmiteForPerson(Guid personId, Guid smiteId);
+        void AddSmiteForPerson(Guid personId, Smite smite);
+        void UpdateSmiteForPerson(Smite smite);
+        void DeleteSmite(Smite smite);
+        bool Save();
+    }
+}
---
+++ Services/SmittenRepository.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Smitten.Api.Entities;
+
+namespace Smitten.Api.Services
+{
+    public class SmittenRepository : ISmittenRepository
+    {
+        private SmittenContext _context;
+
+        public SmittenRepository(SmittenContext context)
+        {
+            _context = context;
+        }
+        public void AddPerson(Person person)
+        {
+            person.Id = Guid.NewGuid();
+            _context.People.Add(person);
+
+            if(person.Smites.Any())
+            {
+                foreach (var smite in person.Smites)
+                {
+                    smite.Id = Guid.NewGuid();
+                }
+            }
+        }
+
+        public void AddSmiteForPerson(Guid personId, Smite smite)
+        {
+            var person = GetPerson(personId);
+            if(person != null)
+            {
+                if (smite.Id == null)
+                    smite.Id = Guid.NewGuid();
+            }
+            person.Smites.Add(smite);
+        }
+
+        public void DeletePerson(Person person)
+        {
+            _context.People.Remove(person);
+        }
+
+        public void DeleteSmite(Smite smite)
+        {
+            _context.Smites.Remove(smite);
+        }
+
+        public IEnumerable<Person> GetPeople()
+        {
+            return _context.People.OrderBy(p => p.FirstName).ThenBy(p => p.LastName);
+        }
+
+        public Person GetPerson(Guid personId)
+        {
+            return _context.People.FirstOrDefault(p => p.Id == personId);
+        }
+
+        public Smite GetSmiteForPerson(Guid personId, Guid smiteId)
+        {
+            return _context.Smites
+                .Where(s => s.PersonId == personId && s.Id == smiteId)
+                .FirstOrDefault();
+        }
+
+        public IEnumerable<Smite> GetSmitesForPerson(Guid personId)
+        {
+            return _context.Smites
+                .Where(s => s.PersonId == personId)
+                .OrderBy(s => s.TimeOfSmite)
+                .ToList();
+        }
+
+        public bool PersonExists(Guid personId)
+        {
+            return _context.People.Any(p => p.Id == personId);
+        }
+
+        public bool Save()
+        {
+            return (_context.SaveChanges() >= 0);
+        }
+
+        public void UpdatePerson(Person person)
+        {
+            // Empty
+        }
+
+        public void UpdateSmiteForPerson(Smite smite)
+        {
+            // Empty
+        }
+    }
+}
